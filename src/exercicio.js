784a3b761c96b2b3a0b1635cc5d777e2e6324253
Rafael Sahyoun Rosa <83467259+rafaelsahyounrosa@users.noreply.github.com>
2024-03-27 08:12:56 -0300

    Finalizando exercicio

---
+++ src/exercicio.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(a, b) {
+    return a * b;
+}
+console.log(multiplicacao(1, 2));
+function saudacao(nome) {
+    return `Olá ${nome}!`;
+}
+console.log(saudacao('Rafael'));
---
+++ src/exercicio.ts
@@ -0,0 +1,11 @@
+function multiplicacao(a:number, b:number):number {
+    return a * b;
+}
+
+console.log(multiplicacao(1,2));
+
+function saudacao(nome:string):string{
+    return `Olá ${nome}!`;
+}
+
+console.log(saudacao('Rafael'));
